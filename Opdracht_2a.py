d409e1d1c614876f16fbc21dc41393da5a2ba956
JasonManason <Jasondemey1995@gmail.com>
2021-02-15 16:33:40 +0100

    Opdracht 2a finished

--- Opdracht_2a.py
+++ Opdracht_2a.py
@@ -9,11 +9,45 @@ products = database.products.find()
 #name and price of first product in collection "products" in database huwebshop:
 print(products[0]["name"], products[0]["price"]["selling_price"])
 
-pr = 0
+
+#prints out the name of the first product with a name starting with "R":
 try:
-    for i in products:
-        product = (products[0]["name"])
-        if product[0] == "R":
-            print(product)
-except:
-    print('Something went wrong')
+    i = 0
+    while True:
+        product = (products[i]["name"])
+
+        if product[0] == "R" or product[0] == "r":
+            print(f'The first product starting with "R": {product}.')
+            break
+
+        i += 1
+except IndexError:
+    print('Something went wrong!')
+
+print((products[0]["price"]["selling_price"]))
+
+
+#prints out the mean price of all products in the database:
+try:
+    try:
+        j = 0
+        price_total = 0
+        while True:
+            price = products[j]["price"]["selling_price"]
+
+            if type(price) == float: # <= if the price is not in cents but euro's it converts it to cents.
+                price = price * 100
+
+            print(price)
+            price_total = price_total + price
+
+            j += 1
+
+    except IndexError:
+        print('Something went wrong with the index!') # <= index out of range
+except KeyError:
+    print('Something went wrong with the price!') # <= product no price
+
+print(f'The total price of all products in the database is: {price_total}')
+
+print(f'The mean price of all products in the database is: {price_total / j}.') #~528 cent
