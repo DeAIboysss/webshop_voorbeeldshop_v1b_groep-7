307886f3b929ce7ed1ef0e7b6ca8bec8b2267848
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-02-15 16:32:07 +0100

    formatieve opdracht 2a af.

--- test_db.py
+++ test_db.py
@@ -20,13 +20,32 @@ except IndexError:
     print("list completed")
 
 # Wat is de gemiddelde prijs van de producten in de database?
-try:
-    i = 0
-    while True:
-        pr = products[i]["name"]
-        if pr[0] == "R":
-            print(pr)
-            break;
-        i+=1
-except IndexError:
-    print("list completed")
+i = 0
+pr = 0
+count = 0
+total = 0
+while True:
+    try:
+        try:
+            pr = products[i]["price"]["selling_price"]
+
+            if type(pr) == int:
+                print(i)
+                total += pr
+                i += 1
+                count += 1
+            else:
+                print(i)
+                i += 1
+                count += 1
+                total += int(pr*100)
+
+
+        except KeyError:
+            print(i)
+            i+=1
+
+    except IndexError :
+        print("list completed")
+        print(f"gem prijs ={total/count} cent")
+        break;
