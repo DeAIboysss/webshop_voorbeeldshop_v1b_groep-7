428eba0c6ec70ae0fdc1df9cf78b1d079ae88fd4
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-03-04 16:37:38 +0100

    formatieve opdracht 2 splitsing op verschillende tabellen met try except code

--- opdracht2c.py
+++ opdracht2c.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 import psycopg2
 
+
+
+
 client = MongoClient()
 database = client.huwebshop
 
@@ -14,12 +17,13 @@ con = psycopg2.connect(
 i = 0
 cur = con.cursor()
 
+
 while True:
+    # product table
     try:
         local_product = db_products[i]
         id = int(local_product['_id'])
         brand = local_product['brand']
-
         name = local_product['name']
         category = local_product['category']
         gender = local_product['gender']
@@ -32,6 +36,20 @@ while True:
         product_size = local_product['product_size']
         product_typename = local_product['product_typename']
 
+        cur.execute(f"INSERT INTO product VALUES {id,name,brand,discription,gender,category,sub_category,sub_sub_category, color,fast_mover,herhaalaankoop,product_size,product_typename};")
+        con.commit()
+    except IndexError:
+        break
+    except ValueError:
+        pass
+    except KeyError:
+        pass
+    except psycopg2.Error:
+        pass
+
+
+    # price table
+    try:
         selling_price = local_product['price']['selling_price']
         cost_price = local_product['price']['cost_price']
         deeplink = local_product['price']['deeplink']
@@ -41,31 +59,65 @@ while True:
         mrsp = local_product['price']['mrsp']
         price_properties = local_product['price']['properties']
 
+        cur.execute(f"INSERT INTO price VALUES {id, cost_price, deeplink, price_discription, selling_price, images, label, mrsp, name, price_properties};")
+        con.commit()
+    except IndexError:
+        break
+    except ValueError:
+        pass
+    except KeyError:
+        pass
+    except psycopg2.Error:
+        pass
+
+
+    # properties table
+    try:
+        properties = []
+        for items in local_product['properties']:
+            items.split(':')
+            properties.append(items)
+        for x in range(0,len(properties)):
+            cur.execute(f"INSERT INTO properties VALUES {id,properties[0],properties[1]}")
+        con.commit()
+    except IndexError:
+        break
+    except ValueError:
+        pass
+    except KeyError:
+        pass
+    except psycopg2.Error:
+        pass
+
+
+    # SM table
+    try:
         is_active = local_product['sm']
         last_updates = local_product['sm']
         rivals_updated = local_product['sm']
         sm_product_type = local_product['sm']
 
-
-
-        properties=[]
-        for items in local_product['properties']:
-            items.split(':')
-            properties.append(items)
-
-
-
-
-
-
-        cur.execute(f"INSERT INTO product VALUES {id,name,brand,discription,gender, category,sub_category,sub_sub_category, color,fast_mover,herhaalaankoop,product_size,product_typename};")
-        con.commit()
-        cur.execute(f"INSERT INTO price VALUES {id, cost_price, deeplink, price_discription, selling_price, images, label, mrsp, name, price_properties};")
-        con.commit()
-        for i in range(0,len(properties)):
-            cur.execute(f"INSERT INTO properties VALUES {id,properties[0],properties[1]}")
-        con.commit()
         cur.execute(f"INSERT INTO sm_product VALUES {id,is_active,last_updates,rivals_updated,sm_product_type}")
+        con.commit()
+    except IndexError:
+        break
+    except ValueError:
+        pass
+    except KeyError:
+        pass
+    except psycopg2.Error:
+        pass
+
+
+    # stock table
+    try:
+        stock_date = local_product['stock']
+        stock_level=local_product['stock_level']
+        for i in range(0,len(local_product['stock'])):
+            stock_date = local_product['stock'][i]
+            stock_level = local_product['stock_level'][i]
+            cur.execute(f"INSERT INTO stock VALUES {id,stock_date,stock_level}")
+            con.commit()
 
     except IndexError:
         break
@@ -77,8 +129,6 @@ while True:
         pass
     finally:
         print(id,"succes")
-
-        con.commit()
         i +=1
 
 
