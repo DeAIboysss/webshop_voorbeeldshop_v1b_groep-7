ae797e01efd6b2b31ff0813fe3e22dc1782d7233
siebe <siebe.vanzwaal@student.hu.nl>
2021-03-08 10:57:38 +0100

    commit profiles werkt 3.5 min

--- tmp.py
+++ tmp.py
@@ -49,10 +49,10 @@ def GetProfiledata():
                 segment = None
 
 
-            biglist.append([profile_id,latest_activity,latest,count,segment])
+            biglist.append((profile_id,latest_activity,latest,count,segment))
 
-            if i % 10000 == 0 :
-                print(i)
+            # if i % 10000 == 0 :
+            #     print(i)
             #     if i != 0 and i != 1000:
             #         print((time0 - time1) -lasttime)
             #         lasttime =time0 - time1
@@ -85,15 +85,25 @@ con = psycopg2.connect(
 cur = con.cursor()
 
 time0 = datetime.datetime.now()
+with open("message (12).txt","r") as ddl:
+    for line in ddl:
+        cur.execute(line)
+
+
+con.commit()
+
 profiles = GetProfiledata()
 #print(profiles)
 
-for p in profiles:
+# for p in profiles:
+#
+#     cur.execute((f"INSERT INTO profile VALUES {p[0],p[1],p[2],p[3],p[4]};").replace("None", "NULL"))
 
-    cur.execute((f"INSERT INTO profile VALUES {p[0],p[1],p[2],p[3],p[4]};").replace("None", "NULL"))
 
 
-#cur.executemany(("INSERT INTO profile VALUES (?,?,?,?,?);",profiles).replace("None", "NULL"))
+
+cur.executemany("INSERT INTO profile VALUES (%s,%s,%s,%s,%s);",profiles)
+
 con.commit()
 cur.close()
 con.close()
