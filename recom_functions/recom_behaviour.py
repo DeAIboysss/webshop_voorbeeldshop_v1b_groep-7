f37bb051ddb872d87bedc521ef973aa51d01ca17
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-04-01 11:22:26 +0200

    to be sure

--- recom_functions/recom_behaviour.py
+++ recom_functions/recom_behaviour.py
@@ -5,10 +5,10 @@ def collect_contentfilter(profileid,cur):
     segment = cur.fetchall()
     segment = list(segment)[0][0]
     print(segment)
+    if type(segment) != type(None):
+        segment = segment.lower()
     if type(segment) == type(None) or segment == 'bouncer' or segment == 'leaver':
         segment = 'buyer'
-    else:
-        segment = segment.lower()
     print(segment)
     cur.execute("SELECT product_id FROM collaborative_recommendations_behaviour WHERE segment = '%s'"%(segment))
     product_ids = cur.fetchall()
